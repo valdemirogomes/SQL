3014518bb8fd21401a9b89965e76fc6d73dc0125
Vania Gomes <vania.vgomes@mercadolivre.com>
2022-11-16 21:04:50 -0300

    Exercicios de banco de dados mysql - bd_rh

---
+++ script_bd_rh.sql
@@ -0,0 +1,34 @@
+create database rh;
+
+use rh;
+
+create table funcionario (
+	id_funcionario int auto_increment ,
+    matricula int not null unique,
+    funcao varchar(30),
+    setor varchar(30),
+    salario double,
+    primary key(id_funcionario)
+);
+
+insert into funcionario (matricula, funcao, setor, salario) values (12, 'Developer', 'TI' , 6.000);
+insert into funcionario (matricula, funcao, setor, salario) values (137896, 'Developer Pleno', 'TI' , 7.000);
+insert into funcionario (matricula, funcao, setor, salario) values (14123, 'Developer FullStack Pleno', 'TI' , 10.000);
+insert into funcionario (matricula, funcao, setor, salario) values (15, 'Developer Junior', 'TI' , 3.000);
+insert into funcionario (matricula, funcao, setor, salario) values (16, 'Developer FullStack Junior', 'TI' , 2.000);
+
+drop table funcionario;
+
+select * from funcionario;
+
+select * from funcionario where salario > 2.000;
+
+select * from funcionario where salario < 2.000;
+
+update funcionario 
+set matricula = 13
+where id_funcionario = 3;
+
+update funcionario 
+set matricula = 14
+where id_funcionario = 4;
