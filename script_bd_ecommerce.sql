0556363ea7b91ba71f6911f84b19291b613d3cf8
valdemirogomes <37006124+valdemirogomes@users.noreply.github.com>
2022-11-17 16:17:36 -0300

    Update script_bd_ecommerce.sql

--- script_bd_ecommerce.sql
+++ script_bd_ecommerce.sql
@@ -4,34 +4,34 @@ use db_ecommerce;
 
 create table tb_produto (
 	id_tbproduto int auto_increment primary key,
-    nome_produto varchar(50),
-    descricao varchar(50),
-    preco double
+	nome_produto varchar(50),
+    	descricao varchar(50),
+    	preco double
 );
 
 insert into tb_produto (nome_produto, descricao, preco) values
-("Tênis Adidas Coreracer Masculino - Preto+Branco", "Tênis masulino", 199.99);
+("Tênis Adidas Coreracer Masculino - Preto+Branco", "Tênis masulino", 199);
 
 insert into tb_produto (nome_produto, descricao, preco) values
-("Tênis Adidas Coreracer Masculino - Preto+Branco", "Tênis masulino", 299.99);
+("Tênis Adidas Coreracer Masculino - Preto+Branco", "Tênis masulino", 299);
 
 insert into tb_produto (nome_produto, descricao, preco) values
-("Tênis Adidas Coreracer Masculino - Preto+Branco", "Tênis masulino", 399.99);
+("Tênis Adidas Coreracer Masculino - Preto+Branco", "Tênis masulino", 399);
 
 insert into tb_produto (nome_produto, descricao, preco) values
-("Tênis Adidas Coreracer Masculino - Preto+Branco", "Tênis masulino", 499.99);
+("Tênis Adidas Coreracer Masculino - Preto+Branco", "Tênis masulino", 499);
 
 insert into tb_produto (nome_produto, descricao, preco) values
-("Tênis Adidas Coreracer Masculino - Preto+Branco", "Tênis masulino", 599.99);
+("Tênis Adidas Coreracer Masculino - Preto+Branco", "Tênis masulino", 599);
 
 insert into tb_produto (nome_produto, descricao, preco) values
-("Tênis Adidas Coreracer Masculino - Preto+Branco", "Tênis masulino", 699.99);
+("Tênis Adidas Coreracer Masculino - Preto+Branco", "Tênis masulino", 699);
 
 insert into tb_produto (nome_produto, descricao, preco) values
-("Tênis Adidas Coreracer Masculino - Preto+Branco", "Tênis masulino", 799.99);
+("Tênis Adidas Coreracer Masculino - Preto+Branco", "Tênis masulino", 799);
 
 insert into tb_produto (nome_produto, descricao, preco) values
-("Tênis Adidas Coreracer Masculino - Preto+Branco", "Tênis masulino", 899.99);
+("Tênis Adidas Coreracer Masculino - Preto+Branco", "Tênis masulino", 899);
 
 select * from tb_produto;
 
